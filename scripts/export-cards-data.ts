dd586d8f4b9bcba3905e3ab2458d4ff379f7d9cd
salty-max <max@jellycat.fr>
2024-04-28 14:39:58 +0200

    fix: build

--- scripts/export-cards-data.ts
+++ scripts/export-cards-data.ts
@@ -1,16 +1,16 @@
-import { PrismaClient } from '@prisma/client';
+// import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-async function exportData() {
-  const cards = await prisma.card.findMany();
-  console.log(JSON.stringify(cards));
-}
+// async function exportData() {
+//   const cards = await prisma.card.findMany();
+//   console.log(JSON.stringify(cards));
+// }
 
-exportData()
-  .catch((e) => {
-    throw e;
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+// exportData()
+//   .catch((e) => {
+//     throw e;
+//   })
+//   .finally(async () => {
+//     await prisma.$disconnect();
+//   });
