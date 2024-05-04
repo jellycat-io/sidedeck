57cbb41476ea1e0699cf8f48aa6a20c5f393f154
salty-max <max@jellycat.fr>
2024-05-04 21:32:56 +0200

    fix: fetch json data

--- data/card.ts
+++ data/card.ts
@@ -1,6 +1,6 @@
 import chalk from 'chalk';
 
-import fs from 'fs';
+import { promises as fs } from 'fs';
 import path from 'path';
 
 import { db } from '@/lib/db';
@@ -13,11 +13,11 @@ import {
 import { ApiCard, LibraryCard, UserCard } from '@/types/cards';
 
 // Construct the path to the JSON file
-const CARDS_JSON_PATH = path.join(process.cwd(), 'data/cards.json');
+const CARDS_JSON_PATH = path.join(process.cwd(), '/app/cards.json');
 
 let cachedCards: ApiCard[] = [];
 
-function loadCards(): ApiCard[] {
+async function loadCards(): Promise<ApiCard[]> {
   try {
     if (cachedCards.length > 0) {
       console.log(chalk.blue(`Using cached cards data...`));
@@ -25,8 +25,10 @@ function loadCards(): ApiCard[] {
     }
 
     console.log(chalk.blue(`Loading cards data...`));
-    const data = fs.readFileSync(CARDS_JSON_PATH, 'utf8');
+    const file = await fs.readFile(CARDS_JSON_PATH, 'utf8');
+    const data = JSON.parse(file);
     console.log(chalk.green('Cards data loaded.'));
+
     console.log(chalk.blue('Caching cards data...'));
     cachedCards = JSON.parse(data);
     console.log(chalk.green('Cards data cached.'));
@@ -51,7 +53,7 @@ export async function getCards() {
 
 export async function getCardsByIds(ids: string[]) {
   try {
-    const cards = loadCards();
+    const cards = await loadCards();
     return cards.filter((card) => ids.includes(card.id));
   } catch (error) {
     throw new Error(
@@ -62,7 +64,7 @@ export async function getCardsByIds(ids: string[]) {
 
 export async function getCardById(id: string) {
   try {
-    const cards = loadCards();
+    const cards = await loadCards();
     const card = cards.find((card) => card.id === id);
     return card;
   } catch (error) {
@@ -85,7 +87,7 @@ export async function getCardNameById(id: string) {
 
 export async function getCardByName(name: string) {
   try {
-    const cards = loadCards();
+    const cards = await loadCards();
     const card = cards.find((card) => card.name === name);
     return card;
   } catch (error) {
@@ -97,7 +99,7 @@ export async function getCardByName(name: string) {
 
 export async function getCardsByQuery(query: string) {
   try {
-    const cards = loadCards();
+    const cards = await loadCards();
     return cards.filter((c) => fuzzyMatch(c.name, query));
   } catch (error) {
     throw new Error(
